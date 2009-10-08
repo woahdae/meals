e0c8bca65861d2a2d9e923606c5e4717f6a3e5b6
Woody Peterson <woody.peterson@gmail.com>
2009-10-08 03:48:03 -0700

    validate presense of item amount and amount_unit

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,8 @@ class Item < ActiveRecord::Base
   belongs_to :recipe
   acts_as_unitable :bulk_qty, :amount
   
+  validates_presence_of :amount, :amount_unit
+  
   def dollars_per_base_unit
     self.bulk_price.to_unit('dollar') / self.bulk_qty_with_unit.to_base
   end
